8fd24aa934ade11aff0e014d2bb1e46fce381465
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2018-08-14 18:45:52 -0500

    Add vue-loader

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span class="count" v-text="count"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
